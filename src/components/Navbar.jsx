a68641eac1873bdb7e43f4b2a7429e113d65f6e8
Camila Florez <camila.rodriguez.florez@gmail.com>
2023-11-15 03:17:00 +0100

    finally done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,20 @@
-function Navbar() {}
+import React from "react";
+import { Link } from "react-router-dom";
+import Home from "../assets/home-icon.png";
+import "../App.css";
+
+function Navbar() {
+  return (
+    <>
+      <Link to="/">
+        <nav className="navbar">
+          <div className="nav-container">
+            <img className="home-icon" src={Home} alt="Home" />
+          </div>
+        </nav>
+      </Link>
+    </>
+  );
+}
 
 export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,41 @@
-function HomePage() {}
+import React from "react";
+import AllBeer from "../assets/beers.png";
+import RandomBeer from "../assets/random-beer.png";
+import NewBeer from "../assets/new-beer.png";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+function HomePage() {
+
+  return (
+    <div className="home-container">
+      <Link to="/beers" className="home-link">
+
+        <img src={AllBeer} alt="All Beers" />
+        <h1>All Beers</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </p>
+      </Link>
+      <Link to="/random-beer" className="home-link">
+        <img src={RandomBeer} alt="randomBeer" />
+        <h1>Random Beer</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </p>
+      </Link>
+      <Link to="/new-beer" className="home-link">
+        <img src={NewBeer} alt="New beer" />
+        <h1>New Beer</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </p>
+      </Link>
+    </div>
+
+  );
+
+
+}
 
 export default HomePage;
